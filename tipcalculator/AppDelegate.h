00194f92361caf5b13457b32656c7ed36a69466f
Dale Hui <dhui@hearsaycorp.com>
2014-02-19 22:25:27 -0800

    Initial Commit

---
+++ tipcalculator/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  tipcalculator
+//
+//  Created by Dale Hui on 2/19/14.
+//  Copyright (c) 2014 Dale Hui. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
