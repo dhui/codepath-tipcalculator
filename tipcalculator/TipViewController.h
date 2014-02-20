7aa56241a28ce596612723f17d21cc7cbff3bb64
Dale Hui <dhui@hearsaycorp.com>
2014-02-19 23:26:36 -0800

    Followed the video tutorial.

---
+++ tipcalculator/TipViewController.h
@@ -0,0 +1,13 @@
+//
+//  TipViewController.h
+//  tipcalculator
+//
+//  Created by Dale Hui on 2/19/14.
+//  Copyright (c) 2014 Dale Hui. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TipViewController : UIViewController
+
+@end
